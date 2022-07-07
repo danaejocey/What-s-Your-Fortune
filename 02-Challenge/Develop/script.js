ce12182b4904d70e5150fe25045693fa6fb51559
unknown <danaejg7@gmail.com>
2022-07-06 18:08:42 -0700

    picture and read me

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,14 +1,80 @@
-// Assignment Code
+function generatePassword() {
+
+  // Characters
+var Lowercase = "abcdefghijklmnopqrstuvwxyz";
+var Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var Numbers = "0123456789";
+var Symbols = "!@#$%^&*_-+=";
+var possibleCharacter = [];
+
+ // get input and validate
+ numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
+ if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+   return "Please choose between 8 and 128 only.";
+ }
+ else {
+   alert("Your password will be " + numberOfCharacters + " characters long.");
+   
+ }
+
+ hasLowercase = confirm("Do you want lowercase characters?");
+ if (hasLowercase) {
+   var turnToLowercase = alert("You have chosen Yes.");
+ }
+ else {
+   alert("You have chosen No.");
+ }
+
+ hasUppercase = confirm("Do you want uppercase characters?");
+ if (hasUppercase) {
+   alert("You have chosen Yes.");
+ }
+ else {
+   alert("You have chosen No");
+ }
+
+ hasNumbers = confirm("Do you want to use numbers?");
+ if (hasNumbers) {
+   alert("You have chosen Yes");
+ }
+ else {
+   alert("You have chosen No.");
+ }
+
+ hasSymbols = confirm("Do you want special characters?");
+ if (hasSymbols) {
+   alert("You have chosen Yes.");
+ }
+ else {
+   alert("You have chosen No.");
+ }
+
+ // combine special characters with `characters`
+
+ if(Lowercase) {
+  possibleCharacter = possibleCharacter.concat(Lowercase)
+ }
+ if(Uppercase) {
+  possibleCharacter = possibleCharacter.concat(Uppercase)
+ }
+ if(Numbers) {
+  possibleCharacter = possibleCharacter.concat(Numbers)
+ }
+ if(Symbols) {
+  possibleCharacter = possibleCharacter.concat(Symbols)
+ }
+}
+ // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # What-s-Your-Fortune
+
+This assignment was to generate a password by answering questions of how long the password should be and if the letters should be capital or lower case and if you want any sumbols. After answering these questions, you should be given a password randomly generated. If you did not say a number between the options given then you would be propmted back and asked to only choose between the two.
+
+My assignemnt asks the full question but does not generate the password. 
+
+Screenshots of my assignment can be found in the folder
+
